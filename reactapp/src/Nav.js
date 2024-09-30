50bd7779e4cbd3cd805f1e0e21a2ad70b089f130
Franck Martigne <f.martigne@gmail.com>
2024-09-30 15:10:11 +0700

    added number of articles in favorites in Nav bar

--- reactapp/src/Nav.js
+++ reactapp/src/Nav.js
@@ -12,7 +12,7 @@ function Nav(props) {
       <nav style={styles.nav} >
         <ul style={styles.leftGroup} >
         <Link to="/screensource"style={styles.item} > <HomeOutlined /> Sources</Link>
-        <Link to="/screenmyarticles"style={styles.item} ><ReadFilled /> My Articles</Link>
+        <Link to="/screenmyarticles"style={styles.item} ><ReadFilled /> ({props.wishList.length})My Articles</Link>
         </ul>
 
         <ul style={styles.rightGroup} >
@@ -29,7 +29,8 @@ function Nav(props) {
 
 function mapStateToProps(state){
   return {
-    username: state.userName
+    username: state.userName,
+    wishList: state.wishList
   }
 }
 
@@ -68,53 +69,3 @@ const styles = {
     fontSize: 15
   }
 }
-
-
-
-
-
-
-
-/*
-import React from 'react';
-import {Link} from 'react-router-dom';
-import { connect } from 'react-redux';
-import './App.css';
-import {Menu} from 'antd';
-import { HomeOutlined, ReadFilled, LogoutOutlined, SettingOutlined} from '@ant-design/icons';
-
-function Nav(props) {
-
-  return (
-    <nav>
-      <Menu mode="horizontal" theme="dark">
-
-        <Menu.Item key="mail">
-          <Link to="/screensource"> <HomeOutlined />Sources</Link>
-        </Menu.Item>
-
-        <Menu.Item key="test">
-          <Link to="/screenmyarticles"><ReadFilled />My Articles</Link>
-        </Menu.Item>
-
-        <Menu.Item key="settings">
-          <Link to={`/user/${props.username}`}><SettingOutlined />My account</Link>
-        </Menu.Item>
-
-        <Menu.Item key="app">
-          <Link to="/"><LogoutOutlined />({props.username})Logout</Link>
-        </Menu.Item>
-        
-      </Menu>
-    </nav>
-  );
-}
-
-function mapStateToProps(state){
-  return {
-    username: state.userName
-  }
-}
-
-export default connect(mapStateToProps, null)(Nav)
-*/
