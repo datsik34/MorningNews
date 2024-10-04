3af3bcb70fbcee5bab45bd9671891edb84cc03bc
Franck Martigne <f.martigne@gmail.com>
2024-10-04 09:33:19 +0700

    upgraded signin/up with: -reset forms on submit -red color error message -email format check -min 3 chars on signup username - min 8 chars on signup password

--- reactapp/src/ScreenHome.js
+++ reactapp/src/ScreenHome.js
@@ -1,59 +1,59 @@
 import React, {useState} from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import { Input, Button } from 'antd';
+import { Input, Button, Form } from 'antd';
 import { Redirect } from 'react-router-dom';
 
 function ScreenHome(props) {
-  const [signUpUsername, setSignUpUsername] = useState('');
-  const [signUpEmail, setSignUpEmail] = useState('');
-  const [signUpPassword, setSignUpPassword] = useState('');
-  const [signInEmail, setSignInEmail] = useState('');
-  const [signInPassword, setSignInPassword] = useState('');
-
   const [userExists, setUserExists] = useState(false);
   const [errorSignIn, setErrorSignIn] = useState('');
   const [errorSignUp, setErrorSignUp] = useState('');
 
-  var signUp = async () => {
-    if(signUpEmail.length === 0 || signUpPassword.length === 0 || signUpUsername.length === 0){
-      setErrorSignUp('empty field(s)');
+  const [formSignIn] = Form.useForm();
+  const [formSignUp] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  var signUp = async (values) => {
+    formSignUp.resetFields();
+    var response = await fetch('/sign-up', {
+      method: 'POST',
+      headers: {'Content-Type':'application/x-www-form-urlencoded'},
+      body: `username=${values.username}&email=${values.email}&password=${values.password}`
+    });
+    const data = await response.json();
+    if(data.result !== null) {
+      setErrorSignUp('all gud m8, registered')
     } else {
-      var response = await fetch('/sign-up', {
-        method: 'POST',
-        headers: {'Content-Type':'application/x-www-form-urlencoded'},
-        body: `username=${signUpUsername}&email=${signUpEmail}&password=${signUpPassword}`
-      });
-      const data = await response.json();
-      if(data.result !== null) {
-        setErrorSignUp('all gud m8, registered')
-      } else {
-        setErrorSignUp('email already registered')
-      }
+      setErrorSignUp('email already registered')
     }
+    
   }
 
-  var signIn = async () => {
-    if(signInEmail.length === 0 || signInPassword.length === 0){
-      setErrorSignIn('empty field(s)');
-    } else {
-      var response = await fetch('/sign-in', {
-        method: 'POST',
-        headers: {'Content-Type':'application/x-www-form-urlencoded'},
-        body: `email=${signInEmail}&password=${signInPassword}`
-      });
-      const data = await response.json()
-      if(data.user) {
-        setUserExists(true)
-        props.addToken(data.user.userToken);
-        props.changeLang(data.user.prefLang);
-        props.getWishlist(data.user.userWishlist);
-        props.addUsername(data.user.userName);
-        props.addEmail(data.user.email);
-        props.addAPI(data.user.APIkey)
-      }
-      else { setErrorSignIn('email or password invalid')}
+  var signIn = async (values) => {
+    formSignIn.resetFields();
+    var response = await fetch('/sign-in', {
+      method: 'POST',
+      headers: {'Content-Type':'application/x-www-form-urlencoded'},
+      body: `email=${values.email}&password=${values.password}`
+    });
+    const data = await response.json()
+    if(data.user) {
+      setUserExists(true)
+      props.addToken(data.user.userToken);
+      props.changeLang(data.user.prefLang);
+      props.getWishlist(data.user.userWishlist);
+      props.addUsername(data.user.userName);
+      props.addEmail(data.user.email);
+      props.addAPI(data.user.APIkey)
     }
+    else { setErrorSignIn('email or password invalid')}
+    
   }
 
   if (userExists) { return <Redirect to='/screensource' /> }
@@ -66,49 +66,73 @@ function ScreenHome(props) {
       <div className="square4"/>
       
       <div className="Login-page" >
-        {/* SIGN-IN */}
-        <form className="Sign">
-          <Input 
-            className="Login-input"
-            placeholder="arthur@lacapsule.com"
-            autoComplete="username"
-            onChange={(e) => setSignInEmail(e.target.value)}
-            value={signInEmail}/>
 
-          <Input.Password
-            className="Login-input"
-            placeholder="password"
-            autoComplete="current-password"
-            onChange={(e) => setSignInPassword(e.target.value)}
-            value={signInPassword}/>
-            <p style={{margin: 0, padding: 10}}>{errorSignIn}</p>
-          <Button type="primary" onClick={() => signIn()} >Sign-in</Button>
-        </form>
+        {/* SIGN-IN */}
+          <Form
+            form={formSignIn}
+            name="signin"
+            initialValues={{ remember: true }}
+            onFinish={signIn}
+            onFinishFailed={onFinishFailed}
+            className="Sign"
+          >
+            <Form.Item
+              name="email"
+              className="Login-input"
+              rules={[ { type:'email', required: true, message: 'Please input your email!' } ]}
+              
+              
+            >
+              <Input placeholder="arthur@lacapsule.com" autoComplete='current-email'/>
+            </Form.Item>
+            <Form.Item
+              name="password"
+              className="Login-input"
+              rules={[ { required: true, message: 'Please input your password!'} ]}
+            >
+              <Input.Password placeholder="password" autoComplete='current-password'/>
+            </Form.Item>
+            <p style={styles.errorMessage}>{errorSignIn}</p>
+            <Form.Item style={{marginBottom: 13}}>
+              <Button type="primary" htmlType="submit"  > Sign-in </Button>
+            </Form.Item>
+          </Form>
 
         {/* SIGN-UP */}
-        <form className="Sign">
-          <Input
-            className="Login-input"
-            placeholder="John Doe" 
-            onChange={(e) => setSignUpUsername(e.target.value)}
-            value={signUpUsername}/>
-
-          <Input
-            className="Login-input"
-            placeholder="email@email.com" 
-            autoComplete="username"
-            onChange={(e) => setSignUpEmail(e.target.value)}
-            value={signUpEmail}/>
-
-          <Input.Password
-            className="Login-input"
-            placeholder="password"
-            autoComplete="current-password"
-            onChange={(e) => setSignUpPassword(e.target.value)}
-            value={signUpPassword}/>
-            <p style={{margin: 0, padding: 10}}>{errorSignUp}</p>
-          <Button type="primary" onClick={() => signUp()}>Sign-up</Button>
-        </form>
+          <Form
+            form={formSignUp}
+            name="signup"
+            initialValues={{ remember: true }}
+            onFinish={signUp}
+            onFinishFailed={onFinishFailed}
+            className="Sign"
+          >
+            <Form.Item
+              name="username"
+              className="Login-input"
+              rules={[ { required: true, message: 'Please input your username!' }, {min: 3} ]}
+            >
+              <Input placeholder="John Doe" autoComplete='current-username'/>
+            </Form.Item>
+            <Form.Item
+              name="email"
+              className="Login-input"
+              rules={[ { type:'email', required: true, message: 'Please input your email!' } ]}
+            >
+              <Input placeholder="email@email.com" autoComplete='current-email'/>
+            </Form.Item>
+            <Form.Item
+              name="password"
+              className="Login-input"
+              rules={[ { required: true, message: 'Please input your password!'}, {min: 8} ]}
+            >
+              <Input.Password placeholder="password" autoComplete='current-password'/>
+            </Form.Item>
+            <p style={styles.errorMessage}>{errorSignUp}</p>
+            <Form.Item style={{marginBottom: 13}}>
+              <Button type="primary" htmlType="submit"> Sign-in </Button>
+            </Form.Item>
+          </Form>
       </div>
     </div>
   );
@@ -142,3 +166,11 @@ function mapDispatchToProps(dispatch){
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ScreenHome);
+
+const styles = {
+  errorMessage: {
+    margin: 0,
+    padding: 10,
+    color: '#EA5354'
+    }
+}
